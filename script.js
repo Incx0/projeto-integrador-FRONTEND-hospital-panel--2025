0944576d133c4cda2d4bafdcc75ee696a75b3557
Your Name <you@example.com>
2025-10-05 10:05:15 -0300

    Refactoring: Separando o projeto hospital-panel como um repositório independente

--- script.js
+++
@@ -1,9 +0,0 @@
-//index
-const btnSalvarToken = document.getElementById("btnSalvarToken");
-const inputInsertId = document.getElementById("inputInsertId");
-
-btnSalvarToken.addEventListener("click", () => {
-    const tokenId = inputInsertId.value;
-    console.log(tokenId)
-    localStorage.setItem("tokenId", tokenId);
-});
--- src/chamar-paciente/chamar-paciente.js
+++
@@ -1,7 +0,0 @@
-//token/ID
-const tokenSalvo = localStorage.getItem("tokenId");
-const inputId = document.getElementById("inputId");
-
-if(tokenSalvo){
-    inputId.value = tokenSalvo;
-}
